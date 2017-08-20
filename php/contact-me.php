4e68c0353d73ae35852d5fd2ff161e0893257cb7
Gareth Davies <daviesgo@gmail.com>
2017-08-20 14:36:26 +0100

    updated email address

--- php/contact-me.php
+++ php/contact-me.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST) {
 
-    $to_Email = "contact@tavolos.com"; // Write your email here
+    $to_Email = "admin@tavolos.com"; // Write your email here
    
     // Use PHP To Detect An Ajax Request
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
